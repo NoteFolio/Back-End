8eaec44ad33b1b531ac66fb8485685b9fdfc9e94
guwls <guswls2654@naver.com>
2025-07-09 23:05:02 +0900

    초기 설정

---
+++ src/main/java/com/example/notefolio/NotefolioApplication.java
@@ -0,0 +1,13 @@
+package com.example.notefolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotefolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotefolioApplication.class, args);
+	}
+
+}
